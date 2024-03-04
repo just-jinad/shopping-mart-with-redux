461babb5f5ecfd991734227861fecf8c7ae46deb
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-03-04 11:43:55 +0100

    reduxApp

--- src/components/cart-tile/index.jsx
+++ src/components/cart-tile/index.jsx
@@ -1,39 +1,34 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import {  removeFromCart } from "../../store/slices/cart-slice";
+import { removeFromCart } from "../../store/slices/cart-slice";
 
 const CartTile = ({ cartItem }) => {
-   const dispatch =  useDispatch()
-  function handleRemoveFromCart() {
-    dispatch (removeFromCart(cartItem.id))
-  }
-  return (
-    <>
-      <div className="flex justify-between items-center p-5 bg-red-500 mt-2 mb-2 rounded-xl">
-        <div className="flex p-3">
-          <img
-            src={cartItem?.image}
-            alt={cartItem?.title}
-            className="h-27 rounded-lg "
-          />
-        <div className="ml-10 self-start space-y-5">
-          <h1 className="text-white font-bold text-xl">{cartItem.title}</h1>
-          <p className="font-etrabold text-white">{cartItem?.price}</p>
-        </div>
-        </div>
+  const dispatch = useDispatch();
 
+  function handleRemoveFromCart() {
+    dispatch(removeFromCart(cartItem.id));
+  }
+
+  return (
+    <div className="flex border border-gray-200 rounded-lg p-4 mb-4">
+      <img
+        src={cartItem?.image}
+        alt={cartItem?.title}
+        className="h-32 w-32 object-cover rounded-md mr-4"
+      />
+      <div className="flex flex-col justify-between">
         <div>
+          <h1 className="text-lg font-semibold mb-1">{cartItem.title}</h1>
+          <p className="text-gray-700 text-sm mb-2">{cartItem?.price}</p>
           <button
-           
             onClick={handleRemoveFromCart}
-            className="bg-red-900 text-white border-2 
-          rounded-lg font-bold p-4"
+            className="text-white bg-red-500 px-3 py-1 rounded-lg font-semibold"
           >
             Remove From Cart
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
